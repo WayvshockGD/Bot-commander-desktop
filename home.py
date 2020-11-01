d5bb8638c0c26b0ce2e89e9c761a816486ab2e58
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-11-01 01:34:07 -0400

    Add files via upload

--- home.py
+++ home.py
@@ -1,10 +1,18 @@
 #modules
 from tkinter import *
+from PIL import Image, ImageTk
 
 #####################
 #text i guess
 
 bcfdText = "Bot Commander Desktop"
+
+#####################
+#colors
+
+color = "#494949"
+barColor = "#0C0C0C"
+white = "#FFFFFF"
 
 #####################
 #code
@@ -13,20 +21,27 @@ root = Tk()
 root.title('BCFD desktop')
 root.geometry("1300x700")
 
-icon = PhotoImage(file = "BCFD-LOGO.png")
+
+icon = PhotoImage(file ="assets/BCFD-LOGO.png")
 root.iconphoto(True, icon)
 
-
-
-title = Label(root, text=bcfdText, anchor='w')
+title = Label(root, text=bcfdText, anchor='w',bg = barColor, fg = white)
 
 title.pack(fill = "x")
 
-tokenBox = Entry(root, width=50)
-tokenBox.pack()
+img = ImageTk.PhotoImage(Image.open("assets/BCFD-Default-icon.png"))
+panel = Label(root, image = img)
+panel.pack(side = "top", fill = "both", expand = "yes")
+
+root.configure(bg=color)
 
 #######################
-
 #on
 
+
 root.mainloop()
+
+
+
+
+
