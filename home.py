73441d6db78c40454eb100ed7f4b68fa5fdaaf84
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-11-04 21:18:46 -0500

    Add files via upload

--- home.py
+++ home.py
@@ -1,18 +1,9 @@
 #modules
 from tkinter import *
 from PIL import Image, ImageTk
-
-#####################
-#text i guess
-
-bcfdText = "Bot Commander Desktop"
-
-#####################
-#colors
-
-color = "#494949"
-barColor = "#0C0C0C"
-white = "#FFFFFF"
+from assets.resources.strings import *
+from assets.resources.colors import *
+from src.styles import *
 
 #####################
 #code
@@ -30,8 +21,11 @@ title = Label(root, text=bcfdText, anchor='w',bg = barColor, fg = white)
 title.pack(fill = "x")
 
 img = ImageTk.PhotoImage(Image.open("assets/BCFD-Default-icon.png"))
-panel = Label(root, image = img)
-panel.pack(side = "top", fill = "both", expand = "yes")
+panel = Label(root, anchor='n', image = img, bg=imgBackroundColor)
+panel.pack(fill = "both", expand = "yes")
+
+tokenBox = Entry(root, width="55", text=tokenText)
+tokenBox.pack(expand = "yes", anchor="center", side='top')
 
 root.configure(bg=color)
 
